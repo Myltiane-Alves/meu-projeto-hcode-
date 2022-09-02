3cf84a6b2be8f1539d41d7548fca6050c525ce00
Myltiane <myltiane.aux@gmail.com>
2022-09-02 12:44:38 -0300

    feat: iniciando a schedule time options

---
+++ Web/src/components/SectionTimeOptions/index.tsx
@@ -0,0 +1,67 @@
+import Calendar from '../Calendar'
+import FooterButton from '../FooterButton'
+import * as S from './styles'
+
+export default function SchedulesNewSection() {
+  return (
+    <S.Container>
+      <S.ContainerTitle>
+        <h1>Escolha o Horário</h1>
+      </S.ContainerTitle>
+      <form>
+        <h3>Sexta, 02 de Agosto 2022</h3>
+        <div className="options">
+          <label>
+            <input type="radio" name="time" value="08:00" checked/>
+            <span>08:00</span>
+          </label>
+
+          <label>
+            <input type="radio" name="time" value="09:00" />
+            <span>09:00</span>
+          </label>
+
+          <label>
+            <input type="radio" name="time" value="10:00" checked/>
+            <span>10:00</span>
+          </label>
+
+          <label>
+            <input type="radio" name="time" value="11:00" checked/>
+            <span>11:00</span>
+          </label>
+          <label>
+            <input type="radio" name="time" value="12:00" checked/>
+            <span>12:00</span>
+          </label>
+          <label>
+            <input type="radio" name="time" value="13:00" checked/>
+            <span>13:00</span>
+          </label>
+          <label>
+            <input type="radio" name="time" value="14:00" checked/>
+            <span>14:00</span>
+          </label>
+          <label>
+            <input type="radio" name="time" value="15:00" checked/>
+            <span>15:00</span>
+          </label>
+          <label>
+            <input type="radio" name="time" value="16:00" checked/>
+            <span>16:00</span>
+          </label>
+          <label>
+            <input type="radio" name="time" value="17:00" checked/>
+            <span>17:00</span>
+          </label>
+          <label>
+            <input type="radio" name="time" value="18:00" checked/>
+            <span>18:00</span>
+          </label>
+
+
+        </div>
+      </form>
+    </S.Container>
+  )
+}
